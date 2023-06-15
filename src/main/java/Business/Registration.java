5665cafcc19bfd532e72468438de2fe6b29841d1
mariestigen <marie.stigen@hotmail.com>
2023-06-15 14:29:09 +0200

    Added guest registration

--- src/main/java/Business/Registration.java
+++ src/main/java/Business/Registration.java
@@ -6,6 +6,7 @@ import Repo.Event.*;
 import Repo.University.*;
 import View.UiHandler;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -23,6 +24,8 @@ public class Registration {
     HashMap<String, EventRegistrationRepo> eventRegistrationRepos = new HashMap<>();
     EventRepo eventRepo;
 
+    ArrayList<Program> programs;
+    ArrayList<Role> roles;
     ArrayList<Person> people;
     ArrayList<Event> events;
     ArrayList<EventRegistration> eventRegistrations;
@@ -43,6 +46,8 @@ public class Registration {
         eventRegistrationRepos.put("staff", new EventRegistrationRepo(eventRepo, staffRepo));
         eventRegistrationRepos.put("student", new EventRegistrationRepo(eventRepo, studentRepo));
 
+        programs = new ArrayList<>();
+        roles = new ArrayList<>();
         people = new ArrayList<>();
         events = new ArrayList<>();
         eventRegistrations = new ArrayList<>();
@@ -60,24 +65,31 @@ public class Registration {
         eventRegistrations.addAll(eventRegistrationRepos.get("guest").getAll());
         eventRegistrations.addAll(eventRegistrationRepos.get("staff").getAll());
         eventRegistrations.addAll(eventRegistrationRepos.get("student").getAll());
+
+        roles.addAll(roleRepo.getAll());
+        programs.addAll(programRepo.getAll());
     }
 
     public void logIn(Scanner scanner){
         System.out.println("Please log in to continue.");
 
         System.out.println("Enter your name:");
-        String name = scanner.nextLine();
+        String name = "";
 
-        //check if user exists in people list, and is a student
-        for (Person person : people) {
-            if (person.getName().equals(name) && person instanceof Student) {
-                user = (Student) person;
-                break;
+        while(user == null){
+            name = scanner.nextLine();
+
+            //check if user exists in people list, and is a student
+            for (Person person : people) {
+                if (person.getName().equalsIgnoreCase(name) && person instanceof Student) {
+                    user = (Student) person;
+                    break;
+                }
             }
-        }
 
-        if (user == null) {
-            System.out.println("User not found.");
+            if (user == null) {
+                System.out.println("User not found.");
+            }
         }
     }
 
@@ -103,10 +115,11 @@ public class Registration {
                 case "1":
                     attendEvent(scanner);
                     break;
-                    /*
+
                 case "2":
                     inviteGuest(scanner);
                     break;
+                    /*
                 case "3":
                     unregisterEvent(scanner);
                     break;
@@ -152,9 +165,12 @@ public class Registration {
         }
     }
 
+    public ArrayList<String> getEventNames(ArrayList<Event> events){
+        return events.stream().map(Event::getName).collect(Collectors.toCollection(ArrayList::new));
+    }
+
     public void attendEvent(Scanner scanner){
         System.out.println("Enter the name of the event you would like to attend:");
-
         //only show events that user is not already registered for
         ArrayList<Event> validEvents = new ArrayList<>();
 
@@ -170,20 +186,103 @@ public class Registration {
                 validEvents.add(event);
             }
         }
-
-        UiHandler.PrintMenuList(validEvents.stream().map(Event::getName).collect(Collectors.toCollection(ArrayList::new)));
+        UiHandler.PrintMenuList(getEventNames(validEvents));
 
         String eventIndex = "";
         Event selectedEvent= null;
 
-        while (!eventIndex.equalsIgnoreCase("Q") || selectedEvent == null){
+        while (!eventIndex.equalsIgnoreCase("Q") && selectedEvent == null){
             eventIndex = scanner.nextLine();
             selectedEvent = events.get(Integer.parseInt(eventIndex) - 1);
+
+            if(selectedEvent == null){
+                System.out.println("Invalid input.");
+            }
         }
 
         EventRegistration eventRegistration = new EventRegistration(selectedEvent, user);
         eventRegistrationRepos.get("student").insert(eventRegistration);
         eventRegistrations.add(eventRegistration);
+    }
+
+
+    public void inviteGuest(Scanner scanner){
+        System.out.println("Which event would you like to invite guests to?");
+        //only show events that user is registered for
+        ArrayList<Event> validEvents = new ArrayList<>();
+
+        for(Event event : events){
+            boolean alreadyRegistered = false;
+            for(EventRegistration eventRegistration : eventRegistrations){
+                if(eventRegistration.getEvent().equals(event) && eventRegistration.getPerson().equals(user)){
+                    alreadyRegistered = true;
+                    break;
+                }
+            }
+            if(alreadyRegistered){
+                validEvents.add(event);
+            }
+        }
+
+        UiHandler.PrintMenuList(getEventNames(validEvents));
+
+        String eventIndex = "";
+        Event selectedEvent= null;
+
+        while (!eventIndex.equalsIgnoreCase("Q") && selectedEvent == null){
+            eventIndex = scanner.nextLine();
+            selectedEvent = events.get(Integer.parseInt(eventIndex) - 1);
+
+            if(selectedEvent == null){
+                System.out.println("Invalid input.");
+            }
+        }
+
+        System.out.println("Enter the name of the guest you would like to invite:");
+        String guestName = scanner.nextLine();
+
+        // check if guest exists in people list, and is a guest (not a student or staff)
+        Guest guest = null;
+
+        for (Person person : people) {
+            if (person.getName().equals(guestName) && person instanceof Guest) {
+                guest = (Guest) person;
+                break;
+            }
+        }
+
+        // if guest exists, see if they are already registered for the event, if not, register them
+        if (guest != null) {
+            boolean alreadyRegistered = false;
+            for(EventRegistration eventRegistration : eventRegistrations){
+                if(eventRegistration.getEvent().equals(selectedEvent) && eventRegistration.getPerson().equals(guest)){
+                    alreadyRegistered = true;
+                    System.out.println("Guest already registered for this event.");
+                    break;
+                }
+            }
+            if(!alreadyRegistered){
+                EventRegistration eventRegistration = new EventRegistration(selectedEvent, guest);
+                eventRegistrationRepos.get("guest").insert(eventRegistration);
+                eventRegistrations.add(eventRegistration);
+            }
+        } else {
+            Role guestRole = roles.stream().filter(role -> role.getRoleName().equalsIgnoreCase("Guest")).findFirst().orElse(null);
+            if(guestRole == null){
+                guestRole = new Role("Guest");
+                roleRepo.insert(guestRole);
+                roles.add(guestRole);
+            }
+
+            guest = new Guest(guestName, guestRole, user);
+            guestRepo.insert(guest);
+            people.add(guest);
+
+            EventRegistration eventRegistration = new EventRegistration(selectedEvent, guest);
+            eventRegistrationRepos.get("guest").insert(eventRegistration);
+            eventRegistrations.add(eventRegistration);
+        }
+
     }
 
 
